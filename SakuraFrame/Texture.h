878ec6bdbf4e84e33a3b240fc3e5e7f23c736193
hazukiaoi <hazuki.aoi1@gmail.com>
2020-06-05 18:26:10 +0800

    修改图片的加载方式，现在读取图片是读取从Unity导出的二进制文件

--- SakuraFrame/Texture.h
+++ SakuraFrame/Texture.h
@@ -1,13 +1,17 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
-#include "stb_image.h"
+//#include "stb_image.h"
 #include "GL/glew.h"
 
 struct Texture
 {
 public:
     int width, height, nrChannels;
+    bool mipmap = true;
     unsigned char *data;
     unsigned int texture;
 
@@ -19,12 +23,14 @@ public:
     ~Texture()
     {
         if (data)
-            stbi_image_free(data);
+            //stbi_image_free(data);
+            free(data);
     }
 };
 
 struct Texture2D : Texture
 {
+
 public:
     Texture2D() {};
     Texture2D(int width, int height)
@@ -33,9 +39,24 @@ public:
         this->height = height;
     }
 
+    //从路径读取二进制格式 
+    //4byte     width
+    //4byte     height
+    //4byte     channels
+    //toEnd     datas 
     Texture2D(const char* path)
     {
-        unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+        std::fstream readImg(path, std::ios::in | std::ios::binary);
+        readImg.read((char*)&width, 4);
+        readImg.read((char*)&height, 4);
+        readImg.read((char*)&nrChannels, 4);
+
+        int len = width * height * nrChannels;
+
+        unsigned char* img = (unsigned char*)malloc(len);
+        readImg.read((char*)img, len);
+        readImg.close();
+        //unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
     }
 
     void SetUp()
@@ -50,14 +71,14 @@ public:
         if (data)
         {
             glTexImage2D(
-                GL_TEXTURE_2D,      //绑定的位置
-                0,                  //Mipmap级数0，基础级数
-                GL_RGB,             //纹理储存格式
-                width,              //宽
-                height,             //高
-                0,                  //历史遗留问题，肯定填0
-                GL_RGB,             //原图格式，此处使用RGB加载
-                GL_UNSIGNED_BYTE,   //储存为char(byte)数组 （上面读入的）
+                GL_TEXTURE_2D,                                  //绑定的位置
+                0,                                              //Mipmap级数0，基础级数
+                nrChannels > 3 ? GL_RGBA : GL_RGB,              //纹理储存格式
+                width,                                          //宽
+                height,                                         //高
+                0,                                              //历史遗留问题，肯定填0
+                nrChannels > 3 ? GL_RGBA : GL_RGB,              //原图格式，此处使用RGB加载
+                GL_UNSIGNED_BYTE,                               //储存为char(byte)数组 （上面读入的）
                 data);
 
             glGenerateMipmap(GL_TEXTURE_2D);    //生成mipmap
@@ -79,6 +100,6 @@ public:
     ~Texture2D()
     {
         if(data)
-            stbi_image_free(data);
+            free(data);
     }
 };
--- SakuraFrame/main.cpp
+++ SakuraFrame/main.cpp
@@ -38,21 +38,44 @@ float deltaTime, lastFrame;
 
 using namespace std;
 
-
-
 int main()
 {
-    int width, height, nrChannels;
-    unsigned char* data = stbi_load("testImg.jpg", &width, &height, &nrChannels, 0);
+    //int _width, _height, _nrChannels;
+    //unsigned char* data = stbi_load("testImg.jpg", &_width, &_height, &_nrChannels, 0);
 
-    fstream fs("Save.dat", ios::out | ios::binary);
+    //cout << "_msize(arr):" << _msize(data) / sizeof(*data) << endl;
+    //cout << "arr size:" << _width * _height * _nrChannels << endl;
 
-    int len = width * height;
 
-    fs.write((char*)data, len);
+   /*fstream fs("Save.dat", ios::out | ios::binary);
 
-    system("pause");
-    return 0;
+    int len = _width * _height * _nrChannels;
+
+    fs.write((char*)data, len * sizeof(char));
+
+    fs.close();
+
+    stbi_image_free(data);
+
+
+
+    fstream ins("Save.dat", ios::in | ios::binary);
+    unsigned char* _d = (unsigned char*)malloc(len);
+
+    ins.read((char*)_d, len);
+
+    ins.close();
+
+    std::cout << "P3\n" << _width << ' ' << _height << "\n255\n";
+
+    for (int i = 0; i < _width * _height; i += 3)
+    {
+        std::cout << (int)_d[i] << ' ' << (int)_d[i + 1] << ' ' << (int)_d[i + 2] << '\n';
+    }
+    free(_d);*/
+
+    //system("pause");
+    //return 0;
 
     //初始化glfw
     glfwInit();
@@ -96,7 +119,41 @@ int main()
     //初始化ImGUI
     InitImGUI(window, glsl_version);
 
-    Vector4 clear_color = Vector4{ 0.1f, 0.5f, 0.9f, 1 };
+    Vector4 clear_color = Vector4{ 0.1f, 0.7f, 0.9f, 1 };
+    
+    //测试贴图显示
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels, len;
+    fstream readImg("Save.dat", ios::in | ios::binary);
+
+    readImg.read((char*)&width, 4);
+    readImg.read((char*)&height, 4);
+    readImg.read((char*)&nrChannels, 4);
+
+    cout << width << "|" << height << "|" << nrChannels << endl;
+
+    len = width * height * nrChannels;
+
+    unsigned char* img = (unsigned char*)malloc(len);
+    //unsigned char* img = stbi_load("testImg.jpg", &width, &height, &nrChannels, 0);
+    readImg.read((char*)img, len);
+    readImg.close();
+
+    if (img)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, img);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    free(img);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -108,36 +165,43 @@ int main()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        glBindTexture(GL_TEXTURE_2D, tex);
         {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            //ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::Begin("Img");
+            ImGui::Image((void*)(intptr_t)tex, ImVec2{ float(width), float(height) });
             ImGui::End();
         }
 
-        {
-            ImGui::SetNextWindowPos({ 0, 20 }, ImGuiCond_Always);
-            ImGui::SetNextWindowSize({ 150, 130 }, ImGuiCond_Always);
-            ImGui::Begin("New Frame");
-            ImGui::Text("This is some useful text.");
-            ImGui::Button("Just A Btn", { 120, 30 });
-            ImGui::End();
-        }
+        //{
+        //    static float f = 0.0f;
+        //    static int counter = 0;
+
+        //    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        //    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        //    //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        //    //ImGui::Checkbox("Another Window", &show_another_window);
+
+        //    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        //    //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        //    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+        //        counter++;
+        //    ImGui::SameLine();
+        //    ImGui::Text("counter = %d", counter);
+
+        //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        //    ImGui::End();
+        //}
+
+        //{
+        //    ImGui::SetNextWindowPos({ 0, 20 }, ImGuiCond_Always);
+        //    ImGui::SetNextWindowSize({ 150, 130 }, ImGuiCond_Always);
+        //    ImGui::Begin("New Frame");
+        //    ImGui::Text("This is some useful text.");
+        //    ImGui::Button("Just A Btn", { 120, 30 });
+        //    ImGui::End();
+        //}
 
         ImGui::Render();
 
@@ -194,6 +258,7 @@ void InitImGUI(GLFWwindow* window, const char* glsl_version)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.IniFilename = NULL;  //不创建imgui.ini的配置文件，记录上一次的ui操作
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
